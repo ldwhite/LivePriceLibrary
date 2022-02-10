577e8e3205e53d0556ca2623b5bd3da24a887c4d
Lindsay Davies-White <81061866+ldwhite@users.noreply.github.com>
2022-02-10 15:18:27 -0500

    Add files via upload

---
+++ live_market_data.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import time
+from datetime import datetime
+from yahoo_fin import stock_info as si
+import csv
+
+ticker = input("Enter desired ticker symbol: ")
+
+price = si.get_live_price(f'{ticker}')
+now = str(datetime.now())
+data = {
+    'Price':price
+}
+print(data)
+
+df = pd.DataFrame(data, index=[now])
+df.to_csv('data.csv')
+
+def save_data():
+    with open(r'data.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Time','Price']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writerow({'Time':now, 'Price':price})
+
+
+while True:
+    now = str(datetime.now())
+    price = si.get_live_price('AAPL')
+    save_data()
+    print(now)
+    print(price)
+
+    time.sleep(10)
